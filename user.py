343a252e8c9e080f9b76e38175985fba59d59691
Amit <amit.hari25@gmail.com>
2022-09-25 21:55:26 +0530

    class added

--- user.py
+++ user.py
@@ -1,10 +1,25 @@
-print("Hello user")
-x=10
-y=100
-r=x+y
-print('result = %s' %r)
-for i in range(1,5):
-    print(i)
 
+class Car:
+    def __init__(self,model = None, color = None):
+        self.model = model
+        self.color = color
+
+    def Show(self):
+        print("Hello")
+        print("Model is", self.model)
+        print("Color is", self.color)
+
+
+#if __name__ == "__main__":
+ferrari = Car("Audi", "blue")
+Bmw = Car("BMW", "white")
+Blank = Car()
+ferrari.Show()
+Bmw.Show()
+Blank.Show()
+
+
+    #rect = Rectangle(10)
+    #print(rect.Area())
 # addig the comment here
 # TOD0 : add proper list here
